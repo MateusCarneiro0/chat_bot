38fd71a8c2da33f7fe8a4eac5df29f290921370e
Mateuspythonuser <mateushenrique20240512@gmail.com>
2026-05-21 15:25:32 -0300

    Initializing a repo and add a navBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [chats, setChats] = useState(["Chat 1"]);
+  const [selectedChat,setSelectedChat] = useState("Chat 1")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>ChatBot</h1>
+      <select value={selectedChat} onChange={(ev) => setSelectedChat(ev.target.value)}>
+        {chats.map((chat) => (
+          <option key={chat} value={chat}>
+            {chat}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
